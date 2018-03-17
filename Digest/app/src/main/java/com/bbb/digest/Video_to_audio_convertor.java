35f74f384c907681028182c63c59f3c1c4846955
Anshul Kishore <anshulkishore310@gmail.com>
2018-03-17 13:00:17 +0530

    Started video to audio convertor activity

---
+++ Digest/app/src/main/java/com/bbb/digest/Video_to_audio_convertor.java
@@ -0,0 +1,13 @@
+package com.bbb.digest;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class Video_to_audio_convertor extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_video_to_audio_convertor);
+    }
+}
