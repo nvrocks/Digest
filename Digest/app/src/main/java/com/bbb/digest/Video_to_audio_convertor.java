51938039ac34077dce9cf0d181022605e1b0890d
Anshul Kishore <anshulkishore310@gmail.com>
2018-03-17 13:02:11 +0530

    Completed video to audio convertor activity

--- Digest/app/src/main/java/com/bbb/digest/Video_to_audio_convertor.java
+++ Digest/app/src/main/java/com/bbb/digest/Video_to_audio_convertor.java
@@ -1,7 +1,17 @@
 package com.bbb.digest;
 
+import android.Manifest;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Toast;
+
+import java.io.File;
+import java.io.InputStream;
+
+import cafe.adriel.androidaudioconverter.AndroidAudioConverter;
+import cafe.adriel.androidaudioconverter.callback.IConvertCallback;
+import cafe.adriel.androidaudioconverter.model.AudioFormat;
 
 public class Video_to_audio_convertor extends AppCompatActivity {
 
@@ -9,5 +19,46 @@ public class Video_to_audio_convertor extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_video_to_audio_convertor);
+
+        Util.requestPermission(this, Manifest.permission.READ_EXTERNAL_STORAGE);
+        Util.requestPermission(this, Manifest.permission.WRITE_EXTERNAL_STORAGE);
+    }
+
+    public void convertAudio(View v){
+        /**
+         *  Update with a valid audio file!
+         *  Supported formats: {@link AndroidAudioConverter.AudioFormat}
+         */
+
+        InputStream in = null;
+
+        /*try {
+            //in = getClass().getResourceAsStream("android.resource://com.example.anshul.speech_to_text/raw/audio_file.flac");
+            in = getAssets().open("out.aac");
+        }
+        catch (Exception e){
+            e.printStackTrace();
+        }*/
+
+        //       File wavFile = new File("android.resource://com.example.anshul.speech_to_text/out.aac");
+        String path=getExternalFilesDir(null).getAbsoluteFile()+"/out.aac";
+        File wavFile = new File(path);
+        IConvertCallback callback = new IConvertCallback() {
+            @Override
+            public void onSuccess(File convertedFile) {
+                Toast.makeText(Video_to_audio_convertor.this, "SUCCESS: " + convertedFile.getPath(), Toast.LENGTH_LONG).show();
+            }
+            @Override
+            public void onFailure(Exception error) {
+                Toast.makeText(Video_to_audio_convertor.this, "ERROR: " + error.getMessage(), Toast.LENGTH_LONG).show();
+            }
+        };
+        Toast.makeText(this, "Converting audio file...", Toast.LENGTH_SHORT).show();
+
+        AndroidAudioConverter.with(this)
+                .setFile(wavFile)
+                .setFormat(AudioFormat.MP3)
+                .setCallback(callback)
+                .convert();
     }
 }
