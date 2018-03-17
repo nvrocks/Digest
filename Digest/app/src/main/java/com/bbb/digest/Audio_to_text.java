28d726474ec83ce6140a5936650084dbe596f270
Anshul Kishore <anshulkishore310@gmail.com>
2018-03-17 08:27:10 +0530

    Added dependencies

---
+++ Digest/app/src/main/java/com/bbb/digest/Audio_to_text.java
@@ -0,0 +1,13 @@
+package com.bbb.digest;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class Audio_to_text extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_audio_to_text);
+    }
+}
